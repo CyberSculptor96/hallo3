3fb623c768d50c5180e4d280cc69cc9e383679a6
CyberSculptor96 <hhj2950526463@gmail.com>
2025-02-25 23:40:50 -1000

    Update config files and scripts

--- scripts/finetune_multi_gpus_s1.sh
+++ scripts/finetune_multi_gpus_s1.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+CUDA_VISIBLE_DEVICES="4,5,6,7"
 echo "RUN on $(hostname), CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
-run_cmd="PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True torchrun --standalone --nproc_per_node=8 hallo3/train_video.py --base configs/cogvideox_5b_i2v_s1.yaml configs/sft_s1.yaml --seed $RANDOM"
+PORT=29500
+run_cmd="PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True torchrun --standalone --nproc_per_node=4 --master_port=$PORT hallo3/train_video.py --wandb --base configs/cogvideox_5b_i2v_s1.yaml configs/sft_s1.yaml --seed $RANDOM"
 
 echo ${run_cmd}
-eval ${run_cmd}
+eval ${run_cmd} > ./runs/logs/stage1.1.log 2>&1
 
 echo "DONE on `hostname`"
--- scripts/inference_long_batch.sh
+++ scripts/inference_long_batch.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
+GPUS=$3
+export CUDA_VISIBLE_DEVICES=$GPUS
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
 environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
 
+export MASTER_PORT=$RANDOM
 run_cmd="$environs python hallo3/sample_video.py --base ./configs/cogvideox_5b_i2v_s2.yaml ./configs/inference.yaml --seed $RANDOM --input-file $1 --output-dir $2"
 
 echo ${run_cmd}
-eval ${run_cmd}
+eval ${run_cmd}> ./runs/logs/inference_testset_${GPUS}.log 2>&1
 
 echo "DONE on `hostname`"
